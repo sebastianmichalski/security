d0cf361acf191d1baf7eaab0b6138dcc622bb4b1
Darshit Chanpura <dchanp@amazon.com>
2023-05-17 13:29:33 -0400

    Indentation fix
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- src/main/java/com/amazon/dlic/auth/http/jwt/AbstractHTTPJwtAuthenticator.java
+++ src/main/java/com/amazon/dlic/auth/http/jwt/AbstractHTTPJwtAuthenticator.java
@@ -43,203 +43,207 @@ import org.opensearch.security.auth.HTTPAuthenticator;
 import org.opensearch.security.user.AuthCredentials;
 
 public abstract class AbstractHTTPJwtAuthenticator implements HTTPAuthenticator {
-	private final static Logger log = LogManager.getLogger(AbstractHTTPJwtAuthenticator.class);
+    public static final int DEFAULT_CLOCK_SKEW_TOLERANCE_SECONDS = 30;
+    private final static Logger log = LogManager.getLogger(AbstractHTTPJwtAuthenticator.class);
+    private static final String BEARER = "bearer ";
+    private static final Pattern BASIC = Pattern.compile("^\\s*Basic\\s.*", Pattern.CASE_INSENSITIVE);
+    private final String jwtHeaderName;
+    private final boolean isDefaultAuthHeader;
+    private final String jwtUrlParameter;
+    private final String subjectKey;
+    private final String rolesKey;
+    private final String requiredAudience;
+    private final String requiredIssuer;
+    private final int clockSkewToleranceSeconds;
+    private final KeyProvider keyProvider;
+    private final JwtVerifier jwtVerifier;
 
-	private static final String BEARER = "bearer ";
-	private static final Pattern BASIC = Pattern.compile("^\\s*Basic\\s.*", Pattern.CASE_INSENSITIVE);
+    public AbstractHTTPJwtAuthenticator(Settings settings, Path configPath) {
+        jwtUrlParameter = settings.get("jwt_url_parameter");
+        jwtHeaderName = settings.get("jwt_header", HttpHeaders.AUTHORIZATION);
+        isDefaultAuthHeader = HttpHeaders.AUTHORIZATION.equalsIgnoreCase(jwtHeaderName);
+        rolesKey = settings.get("roles_key");
+        subjectKey = settings.get("subject_key");
+        clockSkewToleranceSeconds = settings.getAsInt("jwt_clock_skew_tolerance_seconds", DEFAULT_CLOCK_SKEW_TOLERANCE_SECONDS);
+        requiredAudience = settings.get("required_audience");
+        requiredIssuer = settings.get("required_issuer");
 
-	private KeyProvider keyProvider;
-	private JwtVerifier jwtVerifier;
-	private final String jwtHeaderName;
-	private final boolean isDefaultAuthHeader;
-	private final String jwtUrlParameter;
-	private final String subjectKey;
-	private final String rolesKey;
-	private final String requiredAudience;
-	private final String requiredIssuer;
+        try {
+            this.keyProvider = this.initKeyProvider(settings, configPath);
+            jwtVerifier = new JwtVerifier(keyProvider, clockSkewToleranceSeconds);
 
-	public static final int DEFAULT_CLOCK_SKEW_TOLERANCE_SECONDS = 30;
-	private final int clockSkewToleranceSeconds;
+        } catch (Exception e) {
+            log.error("Error creating JWT authenticator. JWT authentication will not work", e);
+            throw new RuntimeException(e);
+        }
+    }
 
-	public AbstractHTTPJwtAuthenticator(Settings settings, Path configPath) {
-		jwtUrlParameter = settings.get("jwt_url_parameter");
-		jwtHeaderName = settings.get("jwt_header", HttpHeaders.AUTHORIZATION);
-		isDefaultAuthHeader = HttpHeaders.AUTHORIZATION.equalsIgnoreCase(jwtHeaderName);
-		rolesKey = settings.get("roles_key");
-		subjectKey = settings.get("subject_key");
-		clockSkewToleranceSeconds = settings.getAsInt("jwt_clock_skew_tolerance_seconds", DEFAULT_CLOCK_SKEW_TOLERANCE_SECONDS);
-		requiredAudience = settings.get("required_audience");
-		requiredIssuer = settings.get("required_issuer");
+    @Override
+    @SuppressWarnings("removal")
+    public AuthCredentials extractCredentials(RestRequest request, ThreadContext context)
+            throws OpenSearchSecurityException {
+        final SecurityManager sm = System.getSecurityManager();
 
-		try {
-			this.keyProvider = this.initKeyProvider(settings, configPath);
-			jwtVerifier = new JwtVerifier(keyProvider, clockSkewToleranceSeconds);
+        if (sm != null) {
+            sm.checkPermission(new SpecialPermission());
+        }
 
-		} catch (Exception e) {
-			log.error("Error creating JWT authenticator. JWT authentication will not work", e);
-			throw new RuntimeException(e);
-		}
-	}
+        AuthCredentials creds = AccessController.doPrivileged(new PrivilegedAction<AuthCredentials>() {
+            @Override
+            public AuthCredentials run() {
+                return extractCredentials0(request);
+            }
+        });
 
-	@Override @SuppressWarnings("removal") public AuthCredentials extractCredentials(RestRequest request, ThreadContext context)
-		throws OpenSearchSecurityException {
-		final SecurityManager sm = System.getSecurityManager();
+        return creds;
+    }
 
-		if (sm != null) {
-			sm.checkPermission(new SpecialPermission());
-		}
+    private AuthCredentials extractCredentials0(final RestRequest request) throws OpenSearchSecurityException {
 
-		AuthCredentials creds = AccessController.doPrivileged(new PrivilegedAction<AuthCredentials>() {
-			@Override public AuthCredentials run() {
-				return extractCredentials0(request);
-			}
-		});
+        String jwtString = getJwtTokenString(request);
 
-		return creds;
-	}
+        if (Strings.isNullOrEmpty(jwtString)) {
+            return null;
+        }
 
-	private AuthCredentials extractCredentials0(final RestRequest request) throws OpenSearchSecurityException {
+        JwtToken jwt;
 
-		String jwtString = getJwtTokenString(request);
+        try {
+            jwt = jwtVerifier.getVerifiedJwtToken(jwtString);
+        } catch (AuthenticatorUnavailableException e) {
+            log.info(e.toString());
+            throw new OpenSearchSecurityException(e.getMessage(), RestStatus.SERVICE_UNAVAILABLE);
+        } catch (BadCredentialsException e) {
+            log.info("Extracting JWT token from {} failed", jwtString, e);
+            return null;
+        }
 
-		if (Strings.isNullOrEmpty(jwtString)) {
-			return null;
-		}
+        JwtClaims claims = jwt.getClaims();
 
-		JwtToken jwt;
+        final String subject = extractSubject(claims);
 
-		try {
-			jwt = jwtVerifier.getVerifiedJwtToken(jwtString);
-		} catch (AuthenticatorUnavailableException e) {
-			log.info(e.toString());
-			throw new OpenSearchSecurityException(e.getMessage(), RestStatus.SERVICE_UNAVAILABLE);
-		} catch (BadCredentialsException e) {
-			log.info("Extracting JWT token from {} failed", jwtString, e);
-			return null;
-		}
+        if (subject == null) {
+            log.error("No subject found in JWT token");
+            return null;
+        }
 
-		JwtClaims claims = jwt.getClaims();
+        final String[] roles = extractRoles(claims);
 
-		final String subject = extractSubject(claims);
+        final AuthCredentials ac = new AuthCredentials(subject, roles).markComplete();
 
-		if (subject == null) {
-			log.error("No subject found in JWT token");
-			return null;
-		}
+        for (Entry<String, Object> claim : claims.asMap().entrySet()) {
+            ac.addAttribute("attr.jwt." + claim.getKey(), String.valueOf(claim.getValue()));
+        }
 
-		final String[] roles = extractRoles(claims);
+        return ac;
 
-		final AuthCredentials ac = new AuthCredentials(subject, roles).markComplete();
+    }
 
-		for (Entry<String, Object> claim : claims.asMap().entrySet()) {
-			ac.addAttribute("attr.jwt." + claim.getKey(), String.valueOf(claim.getValue()));
-		}
+    protected String getJwtTokenString(RestRequest request) {
+        String jwtToken = request.header(jwtHeaderName);
+        if (isDefaultAuthHeader && jwtToken != null && BASIC.matcher(jwtToken).matches()) {
+            jwtToken = null;
+        }
 
-		return ac;
+        if (jwtUrlParameter != null) {
+            if (jwtToken == null || jwtToken.isEmpty()) {
+                jwtToken = request.param(jwtUrlParameter);
+            } else {
+                // just consume to avoid "contains unrecognized parameter"
+                request.param(jwtUrlParameter);
+            }
+        }
 
-	}
+        if (jwtToken == null) {
+            return null;
+        }
 
-	protected String getJwtTokenString(RestRequest request) {
-		String jwtToken = request.header(jwtHeaderName);
-		if (isDefaultAuthHeader && jwtToken != null && BASIC.matcher(jwtToken).matches()) {
-			jwtToken = null;
-		}
+        int index;
 
-		if (jwtUrlParameter != null) {
-			if (jwtToken == null || jwtToken.isEmpty()) {
-				jwtToken = request.param(jwtUrlParameter);
-			} else {
-				// just consume to avoid "contains unrecognized parameter"
-				request.param(jwtUrlParameter);
-			}
-		}
+        if ((index = jwtToken.toLowerCase().indexOf(BEARER)) > -1) { // detect Bearer
+            jwtToken = jwtToken.substring(index + BEARER.length());
+        }
 
-		if (jwtToken == null) {
-			return null;
-		}
+        return jwtToken;
+    }
 
-		int index;
+    @VisibleForTesting
+    public String extractSubject(JwtClaims claims) {
+        String subject = claims.getSubject();
 
-		if ((index = jwtToken.toLowerCase().indexOf(BEARER)) > -1) { // detect Bearer
-			jwtToken = jwtToken.substring(index + BEARER.length());
-		}
+        if (subjectKey != null) {
+            Object subjectObject = claims.getClaim(subjectKey);
 
-		return jwtToken;
-	}
+            if (subjectObject == null) {
+                log.warn("Failed to get subject from JWT claims, check if subject_key '{}' is correct.", subjectKey);
+                return null;
+            }
 
-	@VisibleForTesting public String extractSubject(JwtClaims claims) {
-		String subject = claims.getSubject();
+            // We expect a String. If we find something else, convert to String but issue a
+            // warning
+            if (!(subjectObject instanceof String)) {
+                log.warn(
+                        "Expected type String for roles in the JWT for subject_key {}, but value was '{}' ({}). Will convert this value to String.",
+                        subjectKey,
+                        subjectObject,
+                        subjectObject.getClass());
+                subject = String.valueOf(subjectObject);
+            } else {
+                subject = (String) subjectObject;
+            }
+        }
+        return subject;
+    }
 
-		if (subjectKey != null) {
-			Object subjectObject = claims.getClaim(subjectKey);
+    @SuppressWarnings("unchecked")
+    @VisibleForTesting
+    public String[] extractRoles(JwtClaims claims) {
+        if (rolesKey == null) {
+            return new String[0];
+        }
 
-			if (subjectObject == null) {
-				log.warn("Failed to get subject from JWT claims, check if subject_key '{}' is correct.", subjectKey);
-				return null;
-			}
+        Object rolesObject = claims.getClaim(rolesKey);
 
-			// We expect a String. If we find something else, convert to String but issue a
-			// warning
-			if (!(subjectObject instanceof String)) {
-				log.warn(
-					"Expected type String for roles in the JWT for subject_key {}, but value was '{}' ({}). Will convert this value to String.",
-					subjectKey,
-					subjectObject,
-					subjectObject.getClass());
-				subject = String.valueOf(subjectObject);
-			} else {
-				subject = (String) subjectObject;
-			}
-		}
-		return subject;
-	}
+        if (rolesObject == null) {
+            log.warn(
+                    "Failed to get roles from JWT claims with roles_key '{}'. Check if this key is correct and available in the JWT payload.",
+                    rolesKey);
+            return new String[0];
+        }
 
-	@SuppressWarnings("unchecked") @VisibleForTesting public String[] extractRoles(JwtClaims claims) {
-		if (rolesKey == null) {
-			return new String[0];
-		}
+        String[] roles = String.valueOf(rolesObject).split(",");
 
-		Object rolesObject = claims.getClaim(rolesKey);
+        // We expect a String or Collection. If we find something else, convert to
+        // String but issue a warning
+        if (!(rolesObject instanceof String) && !(rolesObject instanceof Collection<?>)) {
+            log.warn(
+                    "Expected type String or Collection for roles in the JWT for roles_key {}, but value was '{}' ({}). Will convert this value to String.",
+                    rolesKey,
+                    rolesObject,
+                    rolesObject.getClass());
+        } else if (rolesObject instanceof Collection<?>) {
+            roles = ((Collection<String>) rolesObject).toArray(new String[0]);
+        }
 
-		if (rolesObject == null) {
-			log.warn(
-				"Failed to get roles from JWT claims with roles_key '{}'. Check if this key is correct and available in the JWT payload.",
-				rolesKey);
-			return new String[0];
-		}
+        return roles;
+    }
 
-		String[] roles = String.valueOf(rolesObject).split(",");
+    protected abstract KeyProvider initKeyProvider(Settings settings, Path configPath) throws Exception;
 
-		// We expect a String or Collection. If we find something else, convert to
-		// String but issue a warning
-		if (!(rolesObject instanceof String) && !(rolesObject instanceof Collection<?>)) {
-			log.warn(
-				"Expected type String or Collection for roles in the JWT for roles_key {}, but value was '{}' ({}). Will convert this value to String.",
-				rolesKey,
-				rolesObject,
-				rolesObject.getClass());
-		} else if (rolesObject instanceof Collection<?>) {
-			roles = ((Collection<String>) rolesObject).toArray(new String[0]);
-		}
+    @Override
+    public boolean reRequestAuthentication(RestChannel channel, AuthCredentials authCredentials) {
+        final BytesRestResponse wwwAuthenticateResponse = new BytesRestResponse(RestStatus.UNAUTHORIZED, "");
+        wwwAuthenticateResponse.addHeader("WWW-Authenticate", "Bearer realm=\"OpenSearch Security\"");
+        channel.sendResponse(wwwAuthenticateResponse);
+        return true;
+    }
 
-		return roles;
-	}
+    public String getRquiredAudience() {
+        return requiredAudience;
+    }
 
-	protected abstract KeyProvider initKeyProvider(Settings settings, Path configPath) throws Exception;
-
-	@Override public boolean reRequestAuthentication(RestChannel channel, AuthCredentials authCredentials) {
-		final BytesRestResponse wwwAuthenticateResponse = new BytesRestResponse(RestStatus.UNAUTHORIZED, "");
-		wwwAuthenticateResponse.addHeader("WWW-Authenticate", "Bearer realm=\"OpenSearch Security\"");
-		channel.sendResponse(wwwAuthenticateResponse);
-		return true;
-	}
-
-	public String getRquiredAudience() {
-		return requiredAudience;
-	}
-
-	public String getRequiredIssuer() {
-		return requiredIssuer;
-	}
+    public String getRequiredIssuer() {
+        return requiredIssuer;
+    }
 
 }
